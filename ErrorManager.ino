d9444009050212bd8ae5628afe7017f8c9441257
Masyanya228 <marsel.khabibullin.99@gmail.com>
2025-08-20 20:18:45 +0300

    [feat]: errors * Save * Reset * Log to serial * Init EEPROM * REG_GetErrorCount * Store 300 * Store 301

---
+++ ErrorManager.ino
@@ -0,0 +1,74 @@
+void InitEEPROM(){
+  EEPROM.get(0, errors);
+  if(errors[0].code==0){
+    if(isDebug) Serial.println("EEPROM Init");
+    errors[0].code=300;
+    errors[1].code=301;
+    EEPROM.put(0, errors); 
+  }
+}
+
+void SaveError(uint16_t code){
+  logI("encountered", code);
+  int i = IndexOfError(code);
+  errors[i].times++;
+  EEPROM.update(sizeErr*i+2+4, errors[i].times);
+  if(errors[i].tfs==0){
+    errors[i].tfs=millis();
+    EEPROM.update(sizeErr*i+2, errors[i].tfs);
+  }
+}
+
+int IndexOfError(uint16_t code){
+  int len=sizeof(errors) / sizeErr;
+  for(int i=0; i<len; i++)
+  {
+    if(errors[i].code==code){
+      return i;
+    }
+  }
+  return -1;
+}
+
+void ResetError(uint16_t code){
+  int i = IndexOfError(code);
+  errors[i].tfs=0;
+  errors[i].times=0;
+  EEPROM.update(sizeErr*i+2+4, 0);
+  EEPROM.update(sizeErr*i+2, 0);
+  logI("reseted", code);
+}
+
+void LogError(uint16_t code){
+  if(!isDebug) return;
+  
+  int i = IndexOfError(code);
+  Serial.print(errors[i].code);
+  Serial.print(" ");
+  Serial.print(errors[i].tfs);
+  Serial.print(" ");
+  Serial.println(errors[i].times);
+}
+
+void SendHealth(){
+  uint8_t errorCount=0;
+  for(int i=0; i<2; i++){
+    if(errors[i].times>0)
+    {
+      errorCount++;
+    }
+  }
+  I2C_writeAnything(errorCount);
+  I2C_writeAnything(millis());
+}
+
+void SendError(int i){
+  if(i>=2){
+    Wire.write(0);
+    return;
+  }
+  Wire.write(1);
+  I2C_writeAnything(errors[i].code);
+  I2C_writeAnything(errors[i].tfs);
+  I2C_writeAnything(errors[i].times);
+}
--- RearSeatVentilation.ino
+++ RearSeatVentilation.ino
@@ -1,7 +1,8 @@
+#include <EEPROM.h>
 #include <Wire.h>
 
 bool isDebug=false;
-bool isTest=true;
+bool isTest=false;
 
 // адрес
 #define SLAVE_ADDR 20
@@ -13,8 +14,8 @@ bool isTest=true;
 #define REG_GetErrorCount 0x05
 #define REG_GetNextError 0x06
 
-#define PIN_L_Control A6
-#define PIN_R_Control A7
+#define PIN_L_Control 5
+#define PIN_R_Control 6
 
 int L_Mode=0;
 int R_Mode=0;
@@ -23,8 +24,8 @@ byte lowSpeed=125;
 byte midSpeed=180;
 byte highSpeed=255;
 
-byte modeSpeed[]={0, lowSpeed, midSpeed, highSpeed};
-byte modeSeq[]={0, 3, 2, 1};
+int modeSpeed[]={0, lowSpeed, midSpeed, highSpeed};
+int modeSeq[]={0, 3, 2, 1};
 
 // последняя выбранная команда
 // в обработчике приёма
@@ -32,43 +33,78 @@ uint8_t cmd = 0;
 // счётчик сообщений
 uint8_t counter = 0;
 
+uint32_t lastMessage=0;
+//30 секунд таймаута
+#define I2C_NoInputTimeOut 30000;
+bool isOnline=false;
+bool isVoid=false;
+
+struct Error{
+  uint16_t code=0;
+  uint32_t tfs=0;
+  uint8_t times=0;
+};
+
+/* 300 - Связь потеряна
+ * 301 - Связь не установлена
+ */
+Error errors[2];
+int sizeErr;
+int nextError=0;
+
 // обработчик приёма
 void receiveCb(int amount) {
-    cmd = Wire.read();
-    ++counter;
+  cmd = Wire.read();
+  ++counter;
+  lastMessage=millis();
+  isVoid=false;
+  isOnline=true;
 
-    switch (cmd) {
-        case REG_L_MODE:
-            ClickHardware(0);
-            break;
+  switch (cmd) {
+    case REG_L_MODE:
+      ClickHardware(0);
+      break;
 
-        case REG_R_MODE:
-            ClickHardware(1);
-            break;
+    case REG_R_MODE:
+      ClickHardware(1);
+      break;
+        
+    case REG_GetNextError:
+      nextError=Wire.read();
+      break;
 
-        case REG_L_GetStatus: break;
-        case REG_R_GetStatus: break;
-    }
+    case REG_GetErrorCount: break;
+  }
 }
 
 // обработчик запроса
 void requestCb() {
-    switch (cmd) {
-        case REG_L_MODE: 
-        case REG_L_GetStatus:
-          Wire.write(GetIndicator(0));
-          break;
-        case REG_R_MODE:
-        case REG_R_GetStatus:
-          Wire.write(GetIndicator(1));
-          break;
-    }
+  switch (cmd) {
+    case REG_L_MODE: 
+    case REG_L_GetStatus:
+      Wire.write(GetIndicator(0));
+      break;
+    case REG_R_MODE:
+    case REG_R_GetStatus:
+      Wire.write(GetIndicator(1));
+      break;
+    case REG_GetErrorCount:
+      SendHealth();
+      break;
+    case REG_GetNextError:
+      SendError(nextError);
+      break;
+  }
 }
 
 void setup() {
+  sizeErr=sizeof(errors[0]);
   pinMode(PIN_L_Control, OUTPUT);
   pinMode(PIN_R_Control, OUTPUT);
-  Serial.begin(9600);
+  //Serial.begin(9600);
+  InitEEPROM();
+  //SaveError(301);
+  //LogError(301);
   Wire.begin(SLAVE_ADDR);
 
   Wire.onReceive(receiveCb);
@@ -82,14 +118,18 @@ void loop() {
     ClickHardware(0);
     ClickHardware(1);
   }
-}
 
-void CatchErrors(){
-  
-}
-
-void SaveError(){
-  
+  if(!isVoid && !isOnline && lastMessage==0 && millis()>30000)
+  {
+    isVoid=true;
+    SaveError(301);
+  }
+  if(!isVoid && isOnline && millis()-lastMessage>30000)
+  {
+    isOnline=false;
+    isVoid=true;
+    SaveError(300);
+  }
 }
 
 //0-left; 1-right
@@ -97,13 +137,13 @@ void ClickHardware(int seatNum){
   if(seatNum==0)
   {
     L_Mode=GetNextMode(L_Mode);
-    logI("Seat #"+seatNum, L_Mode);
+    logI("Seat #0", L_Mode);
     analogWrite(PIN_L_Control, modeSpeed[L_Mode]);
   }
   else if(seatNum==1)
   {
     R_Mode=GetNextMode(R_Mode);
-    logI("Seat #"+seatNum, R_Mode);
+    logI("Seat #1", R_Mode);
     analogWrite(PIN_R_Control, modeSpeed[R_Mode]);
   }
 }
@@ -115,6 +155,7 @@ int GetNextMode(int mode){
     if(modeSeq[i]==mode){
       break;
     }
+    i++;
   }
   i++;
   if(i>3) i=0;
